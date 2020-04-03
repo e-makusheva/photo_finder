d29da3a7c22a4324663278399b5701effbd15872
Alexander Chernenko <cher.aleksandr@gmail.com>
2020-04-03 21:48:31 +0300

    Дополнительная таблица

--- photo_app/modules.py
+++ photo_app/modules.py
@@ -10,3 +10,10 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username) 
+
+class Service_catalog(db.Model):
+    service_id = db.Column(db.Integer, primary_key=True)
+    service_name = db.Column(db.String(128), index=True, unique=True)
+
+    def __repr__(self):
+        return '<Service_catalog {}>'.format(self.service_name) 
