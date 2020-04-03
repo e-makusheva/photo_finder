54fe4a82cd3d8ddfbe5dc955d7027ac3e42b214c
Alexander Chernenko <cher.aleksandr@gmail.com>
2020-04-03 21:55:16 +0300

    Создана БД

--- photo_app/config.py
+++ photo_app/config.py
@@ -3,5 +3,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 INSTAGRAM_API_KEY='КЛЮЧ'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'photo_app.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'photo_app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- photo_app/modules.py
+++ photo_app/modules.py
@@ -3,10 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(128))
+    roles = db.Column(db.String(64), index=True, unique=True)
 
     def __repr__(self):
         return '<User {}>'.format(self.username) 
