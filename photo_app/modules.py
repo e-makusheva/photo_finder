3621fcb86f9900642bc3e80bccb84fe966b121fb
Alexander Chernenko <cher.aleksandr@gmail.com>
2020-04-07 22:55:59 +0300

    Полная БД

--- photo_app/modules.py
+++ photo_app/modules.py
@@ -18,3 +18,25 @@ class Service_catalog(db.Model):
 
     def __repr__(self):
         return '<Service_catalog {}>'.format(self.service_name) 
+
+class Pricelist(db.Model):
+    price_id = db.Column(db.Integer, primary_key=True)
+    service_id = db.Column(db.Integer, db.ForeignKey('service_catalog.service_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    price = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text)
+
+    def __repr__(self):
+        return '<Pricelist {}>'.format(self.price) 
+
+class Profile(db.Model):
+    profile_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    city = db.Column(db.String(64), nullable=False)
+    about = db.Column(db.Text)
+    Instagram = db.Column(db.Text, nullable=False)
+    contacts = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return '<Profile {}>'.format(self.profile_id) 
+
