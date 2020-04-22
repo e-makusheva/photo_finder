9b7ff37ee5218db2676dae9dcb6cbe232382a4f6
Alexander Chernenko <cher.aleksandr@gmail.com>
2020-04-22 23:38:33 +0300

    Добавлин декоратор админа

--- photo_app/admin/views.py
+++ photo_app/admin/views.py
@@ -1,12 +1,10 @@
 from flask import Blueprint
-from flask_login import current_user, login_required
+
+from photo_app.user.decorators import admin_required
 
 blueprint = Blueprint('admin', __name__, url_prefix='/admin')
 
 @blueprint.route('/')
-@login_required
+@admin_required
 def admin_index():
-    if current_user.is_admin:
-        return 'Доступ разрешён'
-    else:
-        return 'Доступ запрещён'
+    return 'Доступ разрешён'
