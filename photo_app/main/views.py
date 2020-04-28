1ef97365eb69a79f233305726813bbd31aebd731
Alexander Chernenko <cher.aleksandr@gmail.com>
2020-04-29 01:53:49 +0300

    попытка сделать главную страницу

--- photo_app/main/views.py
+++ photo_app/main/views.py
@@ -1,6 +1,4 @@
 from flask import Blueprint, current_app, render_template, request
-from flask_wtf import FlaskForm
-from wtforms.validators import DataRequired
 
 from photo_app import db
 from photo_app.user.models import User
@@ -10,8 +8,6 @@ blueprint = Blueprint('main_page', __name__)
 @blueprint.route('/')
 def index():
     title = 'Photo Finder'
-    return render_template('index.html', page_title=title)
-
-def profiles():
     profile_list = User.query.filter(User.roles == 'photographer')
+    return render_template('index.html', page_title=title)
 
