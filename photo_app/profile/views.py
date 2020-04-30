1d487cbbafce8f62d75eb32a0bc5d3a439e2184e
Ekaterina Makusheva <makusheva.kat@mail.ru>
2020-04-30 21:26:46 +0300

    попытка сделать профили

--- photo_app/profile/views.py
+++ photo_app/profile/views.py
@@ -1,4 +1,4 @@
-from flask import abort, Blueprint, flash, current_app, render_template, redirect, url_for
+from flask import abort, Blueprint, flash, current_app, render_template, redirect, request, url_for
 from flask_login import current_user, login_required
 
 from photo_app.profile.forms import ProfileForm
@@ -12,7 +12,8 @@ def profile(profile_id):
     my_profile = Profile.query.filter(Profile.profile_id == profile_id).first()
     if not my_profile:
         abort(404)
-    return render_template('profile/my_profile.html', profile=profile)
+    profile = ProfileForm(user_id=my_profile.user_id)
+    return render_template('profile/profile.html', profile=my_profile)
 
 @blueprint.route('/edit_profile')
 def edit_profile():
@@ -23,11 +24,11 @@ def edit_profile():
 def process_edit():
     profile_form = ProfileForm()
     if profile_form.validate_on_submit():
-        new_profile = Profile(city=profile_form.city.data, about=profile_form.about.data, Instagram=profile_form.Instagram.data, contacts=profile_form.contacts.data)
+        new_profile = Profile(user_id=current_user.id, city=profile_form.city.data, about=profile_form.about.data, Instagram=profile_form.Instagram.data, contacts=profile_form.contacts.data)
         db.session.add(new_profile)
         db.session.commit()
         flash('Профиль успешно заполнен')
-        #return redirect(url_for 'profile.')
+        return redirect(request.referer)
     flash('Пожалуйста, исправьте ошибки')
     return redirect(url_for('profile.edit_profile'))
 
