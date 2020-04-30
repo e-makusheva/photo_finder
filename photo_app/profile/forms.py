385e88504e955e6e1d4f33e29df63676e487fbce
Ekaterina Makusheva <makusheva.kat@mail.ru>
2020-04-30 20:17:00 +0300

    попытка сделать route для профилей

--- photo_app/profile/forms.py
+++ photo_app/profile/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FileField
+from wtforms import HiddenField, StringField, SubmitField
 from wtforms.validators import DataRequired, ValidationError
 
 class ProfileForm(FlaskForm):
+    user_id = HiddenField('ID пользователя', validators=[DataRequired()])
     city = StringField('Город', validators=[DataRequired()], render_kw={"class": "input100"})
     about = StringField('О себе', validators=[DataRequired()], render_kw={"class": "input100"})
     Instagram = StringField('Адрес страницы в Instagram', validators=[DataRequired()], render_kw={"class": "input100"})
--- photo_app/profile/views.py
+++ photo_app/profile/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, flash, render_template, redirect, url_for
+from flask import abort, Blueprint, flash, current_app, render_template, redirect, url_for
 from flask_login import current_user, login_required
 
 from photo_app.profile.forms import ProfileForm
@@ -6,6 +6,13 @@ from photo_app.profile.models import Profile
 from photo_app import db
 
 blueprint = Blueprint('profile', __name__, url_prefix='/profiles')
+
+@blueprint.route('/<int:profile_id>')
+def profile(profile_id):
+    my_profile = Profile.query.filter(Profile.profile_id == profile_id).first()
+    if not my_profile:
+        abort(404)
+    return render_template('profile/my_profile.html', profile=profile)
 
 @blueprint.route('/edit_profile')
 def edit_profile():
@@ -20,7 +27,7 @@ def process_edit():
         db.session.add(new_profile)
         db.session.commit()
         flash('Профиль успешно заполнен')
-        #return redirect(url_for '')
+        #return redirect(url_for 'profile.')
     flash('Пожалуйста, исправьте ошибки')
     return redirect(url_for('profile.edit_profile'))
 
